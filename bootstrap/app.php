408325e3814bf5a390b614a021f5770280020aef
zdravk00vic <zdravkoovic.nikola@gmail.com>
2025-05-12 02:01:58 +0200

    WIP: {Models/Post and bootstrap/app.php are updated! Swagger file is out.}

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
+        $middleware->api();
         // $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
